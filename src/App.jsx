a201f0a7b581a9d7f00659becb92f5c33f0abd42
Alan Cleetus <alanclee95@gmail.com>
2024-07-03 19:08:34 -0700

    setup firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import MusicComponent from "./components/MusicComponent.jsx";
 import Box from "@mui/material/Box";
 import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import { v4 as uuidv4 } from "uuid";
 import { Howler } from "howler";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./Firebase";
 
 function App() {
   Howler.volume(0.5);
@@ -14,7 +16,27 @@ function App() {
     { uuid: uuidv4(), song: "/sounds/rain.mp3", icon: ThunderstormIcon },
     { uuid: uuidv4(), song: "/sounds/lofi.mp3", icon: MusicNoteIcon },
   ];
-  return (
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const loginAsTestUser = async () => {
+    const testEmail = "test@test.com"; // Replace with your test account email
+    const testPassword = "testpass1!"; // Replace with your test account password
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        testEmail,
+        testPassword
+      );
+      console.log("Test user signed in:", userCredential.user);
+      setLoggedIn(true);
+    } catch (error) {
+      console.error("Error signing in test user:", error);
+    }
+  };
+  return !loggedIn ? (
+    <button onClick={loginAsTestUser}>Login as Test User</button>
+  ) : (
     <Box
       sx={{
         display: "flex",
